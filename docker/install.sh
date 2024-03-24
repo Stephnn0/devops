1c10bf76cb83d843636dbd15aa54111e5892c1b6
susshitechnologies <susshitech@outlook.com>
2024-03-24 18:52:56 -0400

    first commit

---
+++ docker/install.sh
@@ -0,0 +1,12 @@
+echo \
+  "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/ubuntu \
+  $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | \
+  sudo tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+sudo apt-get update
+
+echo "Installing Docker and related packages..."
+
+sudo apt-get install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+echo "Docker installation completed."
