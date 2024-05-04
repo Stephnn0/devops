72de21d329ae786f656118d72f2e7cd1eb177879
stefanoHTB <bocchipinticorp@gmail.com>
2024-05-04 06:02:47 -0400

    wireless

---
+++ linux/net.sh
@@ -0,0 +1,20 @@
+02:00.0 Ethernet controller: Realtek Semiconductor Co., Ltd. RTL8111/8168/8411 PCI Express Gigabit Ethernet Controller (rev 15)
+
+03:00.0 Network controller: Realtek Semiconductor Co., Ltd. RTL8723DE 802.11b/g/n PCIe Adapter
+
+
+
+
+                description: Wireless interface
+                product: RTL8723DE 802.11b/g/n PCIe Adapter
+                vendor: Realtek Semiconductor Co., Ltd.
+                physical id: 0
+                bus info: pci@0000:03:00.0
+                logical name: wlo1
+                version: 00
+                serial: 80:c5:f2:5e:6b:8d
+                width: 64 bits
+                clock: 33MHz
+                capabilities: bus_master cap_list ethernet physical wireless
+                configuration: broadcast=yes driver=rtw_8723de driverversion=6.5.0-28-generic firmware=N/A ip=192.168.1.25 latency=0 link=yes multicast=yes wireless=IEEE 802.11
+                resources: irq:132 ioport:3000(size=256) memory:b1000000-b100ffff
