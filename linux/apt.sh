c0e3bd65a6e3a57e08bba32c972c61c802f27dd0
Di Stephano <104746605+stefanoHTB@users.noreply.github.com>
2024-05-04 22:43:02 -0400

    Create apt.sh

---
+++ linux/apt.sh
@@ -0,0 +1,17 @@
+#deb cdrom:[Debian GNU/Linux 12.5.0 _Bookworm_ - Official amd64 DVD Binary-1 with firmware 20240210-11:28]/ bookworm contrib main non-free-firmware
+#deb http://deb.debian.org/debian/ bullseye main contrib non-free 
+#deb-src http://deb.debian.org/debian bullseye main contrib non-free
+deb http://deb.debian.org/debian/ bookworm contrib main non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm contrib main non-free non-free-firmware
+    
+deb http://deb.debian.org/debian/ bookworm-updates contrib main non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-updates contrib main non-free non-free-firmware
+    
+deb http://deb.debian.org/debian/ bookworm-proposed-updates contrib main non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-proposed-updates contrib main non-free non-free-firmware
+   
+deb http://deb.debian.org/debian/ bookworm-backports contrib main non-free non-free-firmware
+# deb-src http://deb.debian.org/debian/ bookworm-backports contrib main non-free non-free-firmware
+   
+deb http://deb.debian.org/debian-security/ bookworm-security contrib main non-free non-free-firmware
+# deb-src http://deb.debian.org/debian-security/ bookworm-security contrib main non-free non-free-firmware
