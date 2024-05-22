dc48f83bdc3980e4b52cc7c72581b76f95b4ecd5
Di Stephano <104746605+stefanoHTB@users.noreply.github.com>
2024-05-22 18:18:09 -0400

    Create main.sh

---
+++ i3/main.sh
@@ -0,0 +1,9 @@
+mod + enter = terminal
+mod + shift + q = quit
+mod + d = open search/app
+mod + v = next window is vertically added
+mod + arrows = mode
+mod + s = stack windows
+mod + e = back to tile mode
+mod + r = resize window with arrows / esc = exit
+mod numbers = worspaces
