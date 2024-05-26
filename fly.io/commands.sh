7ebdb35fee543555fba1c5b195cdcdd94881a664
Di Stephano <104746605+stefanoHTB@users.noreply.github.com>
2024-05-26 15:51:38 -0400

    Create commands.sh

---
+++ fly.io/commands.sh
@@ -0,0 +1,13 @@
+flyctl auth token ---> for github actions
+
+      - github repo -> settings -> actions -> create repository secret
+
+
+---------------------------------------------
+Create An App: fly launch
+
+Deploy An App: fly deploy
+
+Manage App Secrets: fly secrets
+
+View your App: fly open
