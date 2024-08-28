2494cfb44a08e6374497d4bf3091b01540a04602
Di Stephano <104746605+stefanoHTB@users.noreply.github.com>
2024-08-27 22:01:51 -0400

    Update tmux.sh

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,4 +1,14 @@
+// TMUX LAYERS -----> SESSION, WINDOW
+
 tmux // start session
+
+tmux new -s name-session 
+
+tmux ls // list sessions
+
+tmux a -t 0 // login in specific session
+
+tmux kill-session -t name-session
 
 tmux a // resume session
 
