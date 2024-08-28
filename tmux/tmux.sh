d689b69865e884e9b55702fe5cba2ede73fce3d4
Di Stephano <104746605+stefanoHTB@users.noreply.github.com>
2024-08-27 22:15:45 -0400

    Update tmux.sh

--- tmux/tmux.sh
+++ tmux/tmux.sh
@@ -1,4 +1,6 @@
-// TMUX LAYERS -----> SESSION, WINDOW
+// TMUX LAYERS -----> SESSION, WINDOW, PANES
+
+PREFIX: CTRL B
 
 tmux // start session
 
@@ -15,3 +17,26 @@ tmux a // resume session
 ctrl b d // leave session
 
 ctrl d // end session
+
+
+-------------------------------- PANES
+
+ctrl b , %
+
+ctrl b , "
+
+ctrl b, <- -> ^ // arrows to move
+
+ctrl b , 1 // display indexes of panes
+
+ctrl b , hold ctrl , arrows to change sizes of panes
+
+ctrl b, hold alt , 1 or 2
+
+---------------------------------------- windows
+
+ctrl b , c // new window
+
+ctrl b, n // move around windows
+
+ctrl b, w // move around smartly
